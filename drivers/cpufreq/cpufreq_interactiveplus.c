3cd6274690fb1c8cba98d597c91f8f800305fc45
fake <fake@fake.com>
2016-11-20 21:00:16 +0100

    Fix function name

--- drivers/cpufreq/cpufreq_interactiveplus.c
+++ drivers/cpufreq/cpufreq_interactiveplus.c
@@ -790,7 +790,7 @@ static ssize_t store_hispeed_freq(struct cpufreq_interactiveplus_tunables *tunab
 	int ret;
 	long unsigned int val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->hispeed_freq = val;
@@ -809,7 +809,7 @@ static ssize_t store_go_hispeed_load(struct cpufreq_interactiveplus_tunables
 	int ret;
 	unsigned long val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->go_hispeed_load = val;
@@ -828,7 +828,7 @@ static ssize_t store_min_sample_time(struct cpufreq_interactiveplus_tunables
 	int ret;
 	unsigned long val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->min_sample_time = val;
@@ -846,7 +846,7 @@ static ssize_t store_timer_rate(struct cpufreq_interactiveplus_tunables *tunable
 	int ret;
 	unsigned long val;
 
-	ret = strict_strtoul(buf, 0, &val);
+	ret = kstrtoul(buf, 0, &val);
 	if (ret < 0)
 		return ret;
 	tunables->timer_rate = val;
